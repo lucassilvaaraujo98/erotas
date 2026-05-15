2039f3995ee377fa5d4b383e1fe52a9d19ac6078
Lucas Araujo <165435292+lucassilvaaraujo98@users.noreply.github.com>
2026-05-15 11:51:04 -0300

    Anotações sobre o processo de autenticação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:8080/api"
+    apiUrl: "http://localhost:8080/api" //testar o link assim :http://localhost:8080
     };
