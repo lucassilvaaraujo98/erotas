d7955a442e8789f91308ac8da0d447dda5af35cd
lucassilvaaraujo98 <araujo.lucas.1998@gmail.com>
2026-05-14 21:00:51 -0300

    Primeiro Commit

---
+++ src/main/java/com/erotas/erotas_backend/ErotasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erotas.erotas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErotasBackendApplication.class, args);
+	}
+
+}
