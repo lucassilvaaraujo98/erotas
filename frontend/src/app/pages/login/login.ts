7d2db377203be919a395388a7fa47f5df06456a7
lucassilvaaraujo98 <araujo.lucas.1998@gmail.com>
2026-05-26 21:00:21 -0300

    tela de login ok

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = '';
+  senha = '';
+  erro = '';
+  carregando = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  entrar() {
+    this.erro = '';
+    this.carregando = true;
+
+    this.authService.login({ email: this.email, senha: this.senha })
+      .subscribe({
+        next: () => this.router.navigate(['/caronas']),
+        error: () => {
+          this.erro = 'Email ou senha incorretos.';
+          this.carregando = false;
+        }
+      });
+  }
+}
